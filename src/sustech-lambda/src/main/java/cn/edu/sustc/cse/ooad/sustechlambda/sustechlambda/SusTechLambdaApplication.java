6badb60357b7650589eccc0efd087cc8392c962e
Henrycobaltech <henrytech@outlook.com>
2018-10-22 22:11:16 +0800

    created backend project

---
+++ src/sustech-lambda/src/main/java/cn/edu/sustc/cse/ooad/sustechlambda/sustechlambda/SusTechLambdaApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.sustc.cse.ooad.sustechlambda.sustechlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusTechLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusTechLambdaApplication.class, args);
+	}
+}
