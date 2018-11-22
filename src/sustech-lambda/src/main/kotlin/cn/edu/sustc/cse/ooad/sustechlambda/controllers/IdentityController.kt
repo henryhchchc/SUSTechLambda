c04334860641487828d4ebabe10c33dd6835ec05
Henry Chu <henrytech@outlook.com>
2018-11-22 16:27:34 +0800

    renamed auth controller to identity controller

--- src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/controllers/IdentityController.kt
+++ src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/controllers/IdentityController.kt
@@ -1,6 +1,7 @@
 package cn.edu.sustc.cse.ooad.sustechlambda.controllers
 
 import cn.edu.sustc.cse.ooad.sustechlambda.dtos.LoginDto
+import cn.edu.sustc.cse.ooad.sustechlambda.dtos.UserDto
 import cn.edu.sustc.cse.ooad.sustechlambda.services.IdentityService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
@@ -8,10 +9,7 @@ import io.swagger.annotations.Authorization
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Api(tags = ["Authentication APIs"])
@@ -28,6 +26,12 @@ class AuthController
                                 "access_token" to it
                         ))
                     } ?: ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username of password")
+
+
+    @ApiOperation("Get current user", authorizations = [Authorization("Bearer")], response = UserDto::class)
+    @GetMapping("profile")
+    fun getCurrentUser() = this.identityService.getCurrentUser()!!.let { ResponseEntity.ok(it) }
+
 
     @ApiOperation("Logout", authorizations = [Authorization("Bearer")])
     @PostMapping("logout")
