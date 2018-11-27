bd2d215fa69c36822250e6194de22f1e5ef5f434
DeriZSY <derizsy@foxmail.com>
2018-11-27 18:40:22 +0800

    mode shifting & refactor

--- src/sustech-lambda/src/main/frontend/src/createScript.js
+++ src/sustech-lambda/src/main/frontend/src/createScript.js
@@ -19,6 +19,9 @@ import CodeEditor from './CodeEditor';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import Icon from '@material-ui/core/Icon';
 import DeleteIcon from '@material-ui/icons/Delete';
 import NavigationIcon from '@material-ui/icons/Navigation';
@@ -55,13 +58,15 @@ const param_list = [
 ]
 
 class CreateScripts extends Component{
+
+/************************* General ******************************/
     constructor(props){
         super(props);
         this.state = {
-            title: "Unamed Script ",
+            title: "Untitled Script ",
             description: null,
-            scripts:'import numpy as np\n import torch \n a = np.array([1,2,3])',
-            syntax:'python',
+            scripts:"# Input your codes here :>",
+            syntax:'bash',
             param_list:param_list,
             param1:null,
             param2:null,
@@ -78,56 +83,126 @@ class CreateScripts extends Component{
     };
 
 
-    // Parameter for displaying 
+/************************* Parameter ******************************/
 
-    // Viewing & Input the paramter
-    ShowParam = () => 
-    <div className="paramList">
-    {this.state.param_list.map(item =>{
+    
+    // Editing paramter
+    SingleParamEdit = ({param_name}) =>
+    {
+        const var_type_list = [
+          {  
+           value: 'String '
+          },
+          {
+           value: 'Number '
+          }
+        ]
+        const name_label = param_name + '-name'
+        const type_label = param_name + '-type'
         return (
-            <div key={item.param_name}>
-            <span> {item.param_name} </span>
-            <span> {item.param_type} </span>
-            <span> {item.param_value} </span>
+            <div>
+                <TextField
+                    id = {name_label}
+                    label= {param_name}
+                    value={this.state.param1}
+                    placeholder="value"
+                    onChange={this.handleChange('param1')}
+                    margin="normal"
+                />
+                <TextField
+                id={type_label}
+                select
+                label="Select"
+                value={this.state.currency}
+                onChange={this.handleChange('param1')}
+                placeholder="type"
+                margin="normal"
+                variant="outlined"
+              >
+                { var_type_list.map(option => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+           </div>
+        );
+    }
+
+    SingleParamInput = (param_name)=>{
+    // FIXME: Unfnished codes here
+        const var_type_list = [
+            {  
+             value: 'String '
+            },
+            {
+             value: 'Number '
+            }
+          ]
+          const name_label = param_name + '-name'
+          const type_label = param_name + '-type'
+        return (
+            <div>
+                <TextField
+                    id = {name_label}
+                    label= {param_name}
+                    value={this.state.param1}
+                    placeholder="value"
+                    onChange={this.handleChange('param1')}
+                    margin="normal"
+                />
+                <TextField
+                id={type_label}
+                select
+                label="Select"
+                value={this.state.currency}
+                onChange={this.handleChange('param1')}
+                placeholder="type"
+                margin="normal"
+                variant="outlined"
+              >
+                { var_type_list.map(option => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+           </div>
+        );
+    }
+
+    EditParam = () =>
+    {
+        return (
+            <div className="editParam">
+            <h3>Parameter</h3>
+            <form>
+                <this.SingleParamEdit param_name="website_address" />
+                <br/>
+                <this.SingleParamEdit param_name="video_type" />
+                <br/>
+            </form>
+            </div>
+        );
+    }
+
+        // Viewing & Input the paramter
+    InputParam = () => 
+    // FIXME: Unfnished codes here
+        <div className="paramList">
+        {this.state.param_list.map(item =>{
+        return (
+            <div key={item.param_name}> 
+            <this.SingleParamInput param_name={item.param_name} />
             </div>
        )
-    })}
-    </div>
-
-    // Editing paramter
-    EditParam = () =>
-    <div className="editParam">
-    <form>
-        <TextField
-            id="param1"
-            label="Parameter1:"
-            value={this.state.param1}
-            onChange={this.handleChange('param1')}
-            margin="normal"
-        />
-    <br/>
-        <TextField
-            id="param2"
-            label="Parameter2:"
-            value={this.state.param2}
-            onChange={this.handleChange('param2')}
-            margin="normal"
-        />
-    <br/>
-        <TextField
-            id="param3"
-            label="Parameter3:"
-            value={this.state.param3}
-            onChange={this.handleChange('param3')}
-            margin="normal"
-        /> 
-    </form>
-    </div>
+        })}
+        </div>
 
     // main method of displaying parameter
     ParamDisplay=()=>{
-        if (this.state.mode == "Editing") {return this.EditParam()}
-        if (this.state.mode == "Viewing") {return this.ShowParam()}
+        if (this.state.mode == "Editing") {return <this.EditParam/>}
+        if (this.state.mode == "Viewing") {return <this.InputParam/>}
     }
 
     // For result displaying
@@ -135,7 +210,7 @@ class CreateScripts extends Component{
         if (this.state.mode == "Viewing"){
         return (
         <div className="result">
-        <h2>Result</h2>
+        <h2>Result ></h2>
         <TextField 
         id="result"
         label="result"
@@ -154,7 +229,7 @@ class CreateScripts extends Component{
         }
     }
    
-    // Mode Shifting
+    /************************* Button ******************************/
     modeShift = (new_mode) =>{
       this.setState({mode: new_mode});
     }
@@ -163,43 +238,65 @@ class CreateScripts extends Component{
         if (this.state.mode == "Editing") {this.modeShift("Viewing")};
         if (this.state.mode == "Viewing") {this.modeShift("Editing")};
     }
+
     ButtonDisplay = () =>{
         const lower_right = {float:'right'};
         if (this.state.mode == "Editing"){
             return(
+                <div>
                 <Button 
                 variant="fab" 
                 color="primary" 
                 aria-label="Add" 
-                style={lower_right} 
+                style={lower_right}
                 onClick={()=>this.buttonShift()}
                 >
                     <AddIcon/>
             </Button>
+            </div>
             );
         }
 
         if (this.state.mode == "Viewing"){
             return(
-                <Button 
+             <div>
+             <Button 
+             variant="fab" 
+             aria-label="Delete"
+             style={lower_right}
+             >
+            <DeleteIcon />
+            </Button>
+           <Button 
                 variant="fab" 
                 color="primary" 
-                aria-label="Add" 
-                style={lower_right} 
-                onClick={()=>this.buttonShift()}
+                aria-label="Pub" 
+                style={lower_right}
                 >
-                    <AddIcon/>
-            </Button>
+                Pub
+           </Button>
+           <Button 
+                variant="fab" 
+                color="secondary" 
+                aria-label="Edit" 
+                onClick={()=>this.buttonShift()}
+                style={lower_right}
+                >
+                Edit
+           </Button>
+           </div>
             );
         }
         
     }
 
+    /************************* Script Editor ******************************/
     EditorDisplay = () =>{
         const syntax_ = {float:'right'};
         if (this.state.mode == "Editing"){
             return(
                 <div className="Editor">
+                <form>
                 <TextField 
                     id="code"
                     label="Edit Scripts"
@@ -212,6 +309,7 @@ class CreateScripts extends Component{
                     rowsMax="50"
                     multiline
                 />
+                </form>
                 <input type="text"
                     onChange={this.handleChange('syntax')}
                     value={this.state.syntax}
@@ -223,7 +321,7 @@ class CreateScripts extends Component{
         if (this.state.mode == "Viewing"){
             return(
                 <div className="Editor">
-                <h3> Edit Script >  </h3>
+                <h2> Script >  </h2>
                 <SyntaxHighlighter 
                 language={this.state.syntax}
                 >{this.state.scripts}
@@ -234,26 +332,36 @@ class CreateScripts extends Component{
         }
     }
 
+    // Title Editor
+    EditTitle = () =>
+        <form>
+        <TextField
+        id="title"
+        label="Title"
+        value={this.state.title}
+        onChange={this.handleChange('title')}
+        placeholder="Input your title here"
+        margin="normal"
+        fullWidth
+        />
+        </form>
+    
+    /************************* Title ******************************/
+    TitleDisplay = ()=>{
+        if (this.state.mode == "Editing"){ 
+            return ( 
+                <div className="page title">
+                <h1>{this.state.mode} Mode</h1>
+                <this.EditTitle />
+                </div>
+                );
+            }
+        if (this.state.mode == "Viewing"){ return (<div className="page title"><h1>{this.state.title}</h1> </div>);}
+    }
 
-    // render method 
-    render() {
-        const lower_right = {float:'right'};
-        return(
-            <div className="Basic Infrom">
-            <ButtonAppBar login={true}/>
-            <h1>"blank"</h1>
-            <h1>{this.state.title}</h1>
-            <h2>{this.state.mode} Mode</h2>
-            <form>
-            <TextField
-                id="title"
-                label="Title"
-                value={this.state.title}
-                onChange={this.handleChange('title')}
-                placeholder="Input your title here"
-                margin="normal"
-                fullWidth
-            />
+    /************************* Description ******************************/
+    DescriptionEditor = () => 
+        <form>
             <TextField 
                 id="description"
                 label="Description"
@@ -266,6 +374,41 @@ class CreateScripts extends Component{
                 rowsMax="20"
                 multiline
             />
+        </form>
+    
+
+    // Description display and editor
+    DescriptionDisplay = () =>{
+        if (this.state.mode == "Editing") {
+            return (
+                <div className="Description">
+                <this.DescriptionEditor/>
+                </div>
+            );
+        }
+        
+        if (this.state.mode == "Viewing") {
+            return (
+                <div className="Description">
+                <h2> Decriptioin ></h2>
+                <h4>{this.state.description}</h4>
+                </div>
+            );
+        }
+    }
+
+
+
+    // render method 
+    render() {
+        const lower_right = {float:'right'};
+        return(
+            <div className="Basic Infrom">
+            <ButtonAppBar login={true}/>
+            <h1>"blank"</h1>
+            <this.TitleDisplay/>
+            <this.DescriptionDisplay />
+            <form>
             <br/>
             <this.EditorDisplay />
             <br/>
@@ -273,15 +416,7 @@ class CreateScripts extends Component{
             <br/>
             <this.ParamDisplay />
             <this.ResutlDisplay/>
-            <Button 
-              variant="fab" 
-              color="primary" 
-              aria-label="Add" 
-              style={lower_right} 
-              onClick={()=>this.buttonShift()}
-              >
-                  <AddIcon/>
-             </Button>
+            <this.ButtonDisplay/>
         </div>
 
             )
