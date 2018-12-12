476b1cc61965250371f1b2bca722aea0fff0935c
RainyTong <littlelittletong@gmail.com>
2018-12-12 10:59:47 +0800

    Wyt 1212 update profile with profile page.

--- src/sustech-lambda/src/main/frontend/src/App.js
+++ src/sustech-lambda/src/main/frontend/src/App.js
@@ -7,11 +7,11 @@ import CreateScript from './createScript'
 class App extends Component {
     render() {
         return (
-            /*<PersonalPage/>*/
+            <PersonalPage/>
             // <ScriptList/>
             // <CreateScript/>
             // <CodeEditor/>
-            <HomePage/>
+            //<HomePage/>
         );
     }
 }
--- src/sustech-lambda/src/main/frontend/src/Profile.js
+++ src/sustech-lambda/src/main/frontend/src/Profile.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
-import Typography from "@material-ui/core/Typography/Typography";
-// import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Tabs from '@material-ui/core/Tabs';
-// import Tab from '@material-ui/core/Tab';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import Demo from './demo';
 
+
+import App1 from './profileComponents/App1'
+import './profileComponents/scss/main.scss'
 
 class Profile extends Component{
     render(){
         return(
-            <Demo />
+            <App1/>
+
         )
+
     }
-    // ReactDOM.render(<Demo />, document.querySelector('#root'))
+
 
 }
+
+
+
 export default Profile
