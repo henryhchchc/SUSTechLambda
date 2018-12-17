9c3d2e5a6db5e1e25fd5ab2cdaa84e8b498ea307
Henry Chu <henrytech@outlook.com>
2018-12-17 22:54:04 +0800

    allowed CORS from local front end dev

--- src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/config/WebSecurityConfig.kt
+++ src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/config/WebSecurityConfig.kt
@@ -47,9 +47,9 @@ private class CorsConfiger : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "localhost:3000",
-                        "127.0.0.1:3000",
-                        "[:1]:3000"
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000",
+                        "http://[:1]:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
     }
