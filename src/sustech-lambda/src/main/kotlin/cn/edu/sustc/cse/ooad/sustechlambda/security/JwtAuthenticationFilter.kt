e991db6d7b4198fac8a7033c445622a7d0ddc47c
Henry Chu <henrytech@outlook.com>
2018-12-21 19:36:06 +0800

    add checking for jwt token

--- src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/security/JwtAuthenticationFilter.kt
+++ src/sustech-lambda/src/main/kotlin/cn/edu/sustc/cse/ooad/sustechlambda/security/JwtAuthenticationFilter.kt
@@ -1,6 +1,7 @@
 package cn.edu.sustc.cse.ooad.sustechlambda.security
 
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.MalformedJwtException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import java.security.Key
@@ -15,18 +16,22 @@ class JwtAuthenticationFilter(private val signingKey: Key) : OncePerRequestFilte
             response: HttpServletResponse,
             filterChain: FilterChain
     ) {
-        extractToken(request)?.let {
-            Jwts.parser()
-                    .setSigningKey(this.signingKey)
-                    .parseClaimsJws(it)
-                    .body
-        }?.let {
-            JwtAuthenticationToken(it)
-        }.also {
-            SecurityContextHolder.getContext().authentication = it
+        try {
+            extractToken(request)?.let {
+                Jwts.parser()
+                        .setSigningKey(this.signingKey)
+                        .parseClaimsJws(it)
+                        .body
+            }?.let {
+                JwtAuthenticationToken(it)
+            }.also {
+                SecurityContextHolder.getContext().authentication = it
+            }
+            filterChain.doFilter(request, response)
+        } catch (ex: MalformedJwtException) {
+            response.sendError(401, "Invalid access token")
         }
 
-        filterChain.doFilter(request, response)
     }
 }
 
